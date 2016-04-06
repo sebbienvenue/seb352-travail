749a10ece37b9bb827f531ae23d3c64bb086194e
sebbienvenue <sebastien.bienvenue@epfl.ch>
2016-04-06 10:59:18 +0200

    commmit success

---
+++ Belotte/carte.py
@@ -0,0 +1,32 @@
+#a pour but de generer toute sortre de carte et proprietes
+
+
+class Paquet(object):
+    def __init__(self):
+        self.cc = ["Pic", "Carreau", "Coeur","Trefle"]
+        self.nn = ["7", "8", "9", "10", "Valet", "Dame", "Roi", "As"]
+        self.valeur = [0, 0, 0, 10, 2, 3, 4, 11, 14, 20]
+        self.paquet =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    path=0
