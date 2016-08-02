4ffb5f07ce2b226a707d9028174ef55fb3021121
Fernando on behalf of tutorial authors <fernando.gargiulo@epfl.ch>
2016-08-02 14:02:43 +0000

    up cleanned

--- essais-tuto/ex-query.py
+++ essais-tuto/ex-query.py
@@ -3,9 +3,21 @@
 from aiida.orm.querybuilder import QueryBuilder
 from aiida.orm.data.remote import RemoteData
 
-qb=QueryBuilder()
-qb.append(Group, filters={"name":{"in": ["tutorial_pbesol", "tutorial_lda", "tutorial_pbe"]   }})
+StructureData=DataFactory("structure")
+ParameterData=DataFactory("parameter")
 
-for i in qb.iterall():
-	print i
-res=qb.iterdict()
+qb=QueryBuilder()
+qb.append(RemoteData, tag="remote", project=["*"])
+qb.append(Group,group_of="remote",
+	filters={"name":{"in": ["tutorial_pbesol", "tutorial_lda", "tutorial_pbe"]   }})
+
+qb.append(ParameterData, project=["attributes.energy_smearing"],
+
+
+#qb.append(ParameterData, project=["attributes.energy_smearing"],
+#	 filters={"id":{"==":1}} )
+
+#qb.append(ParameterData, project=["attributes.energy_smearing"]
+
+qb.all()
+
